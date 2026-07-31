96fa682368837a637406fc606415e8896c1fac61
Viktor Haag <viktor.haag.1989@gmail.com>
2026-07-31 22:46:32 +0200

    chore: wip

--- nextcloud/skjalfsearch/webpack.config.js
+++ nextcloud/skjalfsearch/webpack.config.js
@@ -1,13 +1,35 @@
 const path = require('path')
-const webpackConfig = require('@nextcloud/webpack-vue-config')
+const { VueLoaderPlugin } = require('vue-loader')
 
 module.exports = {
-  ...webpackConfig,
+  mode: 'production',
   entry: {
     app: path.join(__dirname, 'src', 'main.js'),
   },
   output: {
     path: path.join(__dirname, 'js'),
-    filename: 'app.js',
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: ['vue-style-loader', 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['vue-style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
+  resolve: {
+    extensions: ['.js', '.vue'],
   },
 }
